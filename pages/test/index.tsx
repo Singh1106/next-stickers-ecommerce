a4c68757595abb5b561e3d9dfe6956a6f8b9698f
Jaswinder Singh <Jaswindersingh11061998@gmail.com>
2022-11-13 15:52:10 +0530

    added toast notification

--- pages/test/index.tsx
+++ pages/test/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import useAuthStore from "../../src/store";
 
-const index = () => {
+const Test = () => {
   const user = useAuthStore((state: any) => state.user);
   return <div>{JSON.stringify(user)}</div>;
 };
 
-export default index;
+export default Test;
