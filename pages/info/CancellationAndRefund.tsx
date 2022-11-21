a899b6c207883eb549a85dabce3b4c102b4b5bf2
Jaswinder Singh <Jaswindersingh11061998@gmail.com>
2022-11-21 11:43:52 +0530

    added info section

---
+++ pages/info/CancellationAndRefund.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CancellationAndRefund = () => {
+  return <div>CancellationAndRefund</div>;
+};
+
+export default CancellationAndRefund;
---
+++ pages/info/ShippingAndDelivery.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ShippingAndDelivery = () => {
+  return <div>ShippingAndDelivery</div>;
+};
+
+export default ShippingAndDelivery;
