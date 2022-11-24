70211ae506e389ed74865c5e0ad5539f02e29577
Jaswinder Singh <Jaswindersingh11061998@gmail.com>
2022-11-25 03:43:26 +0530

    domain text commit

--- src/templates/mail.ts
+++ src/templates/mail.ts
@@ -15,6 +15,7 @@ export const getMailOptions = (type: string, email: string, options?: any) => {
         to: email,
         subject: `Verification Email for nextCommerce`,
         html: `Okay, nice. You wanna verify your email. Click here <a href="${options?.domain}/emailVerificationResult/${options.verificationCode}" target="_blank" rel="noopener noreferrer">YO, LINK IS HERE</a>.
+        <p>${options?.domain}/emailVerificationResult/${options.verificationCode}</p>
         <p>Your email is valid till ${options?.date}.</p>
         <p>Hurry up.</p>`,
       };
